98684f890f7cb906e4e2ac8161edd04784224559
Tinh2T <trantrungtinh4954@gmail.com>
2021-08-08 16:08:59 +0700

    new(project): :tada: Start new app

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a50896ae49839f59853a3dcff853cf8b'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bee31d9-7bb4-43a5-a4a2-cdf147175c8b'
+};

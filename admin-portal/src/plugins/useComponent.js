6660b9baa0f20df23cab0107848fb5c96e9305ee
Tinh Tran <tttinh@sk-global.biz>
2022-03-10 11:30:21 +0700

    config(server): Upgrade server version

---
+++ admin-portal/src/plugins/useComponent.js
@@ -0,0 +1,6 @@
+import VueFormJsonSchema from "vue-form-json-schema/dist/vue-form-json-schema.esm";
+const useComponent = app => {
+  app.component('vue-form-json-schema', VueFormJsonSchema)
+}
+
+export default useComponent
